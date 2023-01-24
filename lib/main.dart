e1c2221b56c113c61792f72cf89adfc2741226c1
Nurdaulet <nurik9293709@gmail.com>
2023-01-25 05:25:32 +0600

    main.dart: kReleaseMode fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:journal/src/application.dart';
 import 'package:journal/src/locator.dart' as di;
 
 void main() async {
-  if (0 > 1) {
-    // kReleaseMode
+  if (kReleaseMode) {
     await dotenv.load(fileName: ".env.production");
   } else {
     await dotenv.load(fileName: ".env");
