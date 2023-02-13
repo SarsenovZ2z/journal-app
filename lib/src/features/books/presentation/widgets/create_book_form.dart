e491ff5570f38aae7517efb201404d0b26256189
Nurdaulet <nurik9293709@gmail.com>
2023-02-14 03:13:50 +0600

    bottom sheet scroll fix

--- lib/src/features/books/presentation/widgets/create_book_form.dart
+++ lib/src/features/books/presentation/widgets/create_book_form.dart
@@ -31,111 +31,113 @@ class _CreateBookFormState extends State<CreateBookForm> {
       create: (context) => sl<CreateBookCubit>(),
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              constraints: const BoxConstraints(maxWidth: 500),
-              child: Form(
-                key: _formKey,
-                child: BlocBuilder<CreateBookCubit, CreateBookState>(
-                    builder: (context, createBookState) {
-                  return Column(
-                    children: [
-                      Container(
-                        constraints: const BoxConstraints(maxWidth: 300),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).highlightColor,
-                            style: BorderStyle.solid,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                constraints: const BoxConstraints(maxWidth: 500),
+                child: Form(
+                  key: _formKey,
+                  child: BlocBuilder<CreateBookCubit, CreateBookState>(
+                      builder: (context, createBookState) {
+                    return Column(
+                      children: [
+                        Container(
+                          constraints: const BoxConstraints(maxHeight: 200),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Theme.of(context).highlightColor,
+                              style: BorderStyle.solid,
+                            ),
+                          ),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: _image != null
+                                ? Image.network(
+                                    _image!.path,
+                                    fit: BoxFit.cover,
+                                  )
+                                : null,
                           ),
                         ),
-                        child: AspectRatio(
-                          aspectRatio: 1,
-                          child: _image != null
-                              ? Image.network(
-                                  _image!.path,
-                                  fit: BoxFit.cover,
-                                )
-                              : null,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      ElevatedButton.icon(
-                        onPressed: () async {
-                          final XFile? pickedFile = await _picker.pickImage(
-                            source: ImageSource.gallery,
-                          );
-                          if (pickedFile != null) {
-                            setState(() {
-                              _image = pickedFile;
-                            });
-                          }
-                        },
-                        label: const Text('Select image'),
-                        icon: const Icon(Icons.image),
-                      ),
-                      const SizedBox(height: 30),
-                      TextFormField(
-                        controller: _nameController,
-                        enabled: !_isLoading,
-                        validator: (String? value) =>
-                            _nameValidator(value, context),
-                        decoration: const InputDecoration(
-                          label: Text('Name'),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        controller: _passwordController,
-                        enabled: !_isLoading,
-                        obscureText: _isObscurePasswordEnabled,
-                        validator: (String? value) =>
-                            _passwordValidator(value, context),
-                        decoration: InputDecoration(
-                          label: const Text('Password'),
-                          suffixIcon: InkWell(
-                            onTap: () {
+                        const SizedBox(height: 5),
+                        ElevatedButton.icon(
+                          onPressed: () async {
+                            final XFile? pickedFile = await _picker.pickImage(
+                              source: ImageSource.gallery,
+                            );
+                            if (pickedFile != null) {
                               setState(() {
-                                _isObscurePasswordEnabled =
-                                    !_isObscurePasswordEnabled;
+                                _image = pickedFile;
                               });
-                            },
-                            child: Icon(_isObscurePasswordEnabled
-                                ? Icons.visibility
-                                : Icons.visibility_off),
+                            }
+                          },
+                          label: const Text('Select image'),
+                          icon: const Icon(Icons.image),
+                        ),
+                        const SizedBox(height: 30),
+                        TextFormField(
+                          controller: _nameController,
+                          enabled: !_isLoading,
+                          validator: (String? value) =>
+                              _nameValidator(value, context),
+                          decoration: const InputDecoration(
+                            label: Text('Name'),
+                            border: OutlineInputBorder(),
                           ),
-                          border: const OutlineInputBorder(),
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: _isLoading
-                            ? null
-                            : () async {
-                                await _load(_createBook(context));
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: _passwordController,
+                          enabled: !_isLoading,
+                          obscureText: _isObscurePasswordEnabled,
+                          validator: (String? value) =>
+                              _passwordValidator(value, context),
+                          decoration: InputDecoration(
+                            label: const Text('Password'),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  _isObscurePasswordEnabled =
+                                      !_isObscurePasswordEnabled;
+                                });
                               },
-                        child: Container(
-                          alignment: Alignment.center,
-                          constraints: const BoxConstraints(minHeight: 50),
-                          child: _isLoading
-                              ? const SizedBox(
-                                  width: 20,
-                                  height: 20,
-                                  child: CircularProgressIndicator(),
-                                )
-                              : const Text('Submit'),
+                              child: Icon(_isObscurePasswordEnabled
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                            ),
+                            border: const OutlineInputBorder(),
+                          ),
                         ),
-                      ),
-                    ],
-                  );
-                }),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: _isLoading
+                              ? null
+                              : () async {
+                                  await _load(_createBook(context));
+                                },
+                          child: Container(
+                            alignment: Alignment.center,
+                            constraints: const BoxConstraints(minHeight: 50),
+                            child: _isLoading
+                                ? const SizedBox(
+                                    width: 20,
+                                    height: 20,
+                                    child: CircularProgressIndicator(),
+                                  )
+                                : const Text('Submit'),
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
